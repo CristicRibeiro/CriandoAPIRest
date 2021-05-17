cf2797860618e9107a6e41b3b5e34b67ad32e905
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2021-05-17 19:11:56 -0300

    Add files via upload

---
+++ macaancrm-api/src/test/java/com/macaan/crm/MacaancrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macaan.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacaancrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
